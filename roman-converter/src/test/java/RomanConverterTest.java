8b0f43ae7c442aa39920acfe234be27684c697f3
holi-java <32414154@qq.com>
2016-09-21 01:34:26 +0800

    shortened tests

--- roman-converter/src/test/java/RomanConverterTest.java
+++ roman-converter/src/test/java/RomanConverterTest.java
@@ -22,51 +22,21 @@ public class RomanConverterTest {
 
   @Parameterized.Parameters public static Collection<Object[]> data() {
     return Arrays.asList(new Object[][] {
-        new Object[] {0, ""}
+        new Object[] {0, ""},
+        new Object[] {1, "I"},
+        new Object[] {2, "II"},
+        new Object[] {3, "III"},
+        new Object[] {4, "IV"},
+        new Object[] {5, "V"},
+        new Object[] {6, "VI"},
+        new Object[] {10, "X"},
+        new Object[] {9, "IX"},
+        new Object[] {50, "L"},
+        new Object[] {40, "XL"},
     });
   }
 
   @Test public void convert() throws Exception {
     assertThat(RomanConverter.convert(number), equalTo(graph));
-  }
-
-  @Test public void one() throws Exception {
-    assertThat(RomanConverter.convert(1), equalTo("I"));
-  }
-
-  @Test public void two() throws Exception {
-    assertThat(RomanConverter.convert(2), equalTo("II"));
-  }
-
-  @Test public void three() throws Exception {
-    assertThat(RomanConverter.convert(3), equalTo("III"));
-  }
-
-  @Test public void four() throws Exception {
-    assertThat(RomanConverter.convert(4), equalTo("IV"));
-  }
-
-  @Test public void five() throws Exception {
-    assertThat(RomanConverter.convert(5), equalTo("V"));
-  }
-
-  @Test public void six() throws Exception {
-    assertThat(RomanConverter.convert(6), equalTo("VI"));
-  }
-
-  @Test public void ten() throws Exception {
-    assertThat(RomanConverter.convert(10), equalTo("X"));
-  }
-
-  @Test public void nine() throws Exception {
-    assertThat(RomanConverter.convert(9), equalTo("IX"));
-  }
-
-  @Test public void fifty() throws Exception {
-    assertThat(RomanConverter.convert(50), equalTo("L"));
-  }
-
-  @Test public void forty() throws Exception {
-    assertThat(RomanConverter.convert(40), equalTo("XL"));
   }
 }
