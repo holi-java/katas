3dd1551fdabf82c84282a5a755a41e9b569f76cb
holi-java <32414154@qq.com>
2016-09-20 21:30:27 +0800

    pass:fn(2)

--- prime-factors/src/test/java/PrimeFactorsTest.java
+++ prime-factors/src/test/java/PrimeFactorsTest.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 import java.util.Collections;
 import org.junit.Test;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,5 +13,9 @@ import static org.junit.Assert.assertThat;
 public class PrimeFactorsTest {
   @Test public void noFactors() throws Exception {
     assertThat(PrimeFactors.of(1), is(emptyList()));
+  }
+
+  @Test public void primeNumber() throws Exception {
+    assertThat(PrimeFactors.of(2), is(singletonList(2)));
   }
 }
