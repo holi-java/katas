7c5edf1c4c9c3f9ba16c62ba5e8ed5351ab7efbe
holi-java <32414154@qq.com>
2016-09-21 00:54:41 +0800

    what's happens if we let's the Radix to render it's occurrences?

---
+++ roman-converter/src/main/java/Radix.java
@@ -0,0 +1,22 @@
+/**
+ * Created by selonj on 16-9-21.
+ */
+public enum Radix {
+  V(5),
+  IV(4),
+  I(1);
+
+  private final int radix;
+
+  Radix(int radix) {
+    this.radix = radix;
+  }
+
+  public int reduce(int number) {
+    return number - this.radix;
+  }
+
+  public boolean includedWith(int number) {
+    return this.radix <= number;
+  }
+}
--- roman-converter/src/main/java/RomanConverter.java
+++ roman-converter/src/main/java/RomanConverter.java
@@ -4,30 +4,10 @@
 public class RomanConverter {
   public static String convert(int number) {
     for (Radix radix : Radix.values()) {
-      if (radix.closest(number)) {
+      if (radix.includedWith(number)) {
         return radix.name() + convert(radix.reduce(number));
       }
     }
     return "";
-  }
-
-  private enum Radix {
-    V(5),
-    IV(4),
-    I(1);
-
-    private final int radix;
-
-    Radix(int radix) {
-      this.radix = radix;
-    }
-
-    private int reduce(int number) {
-      return number - this.radix;
-    }
-
-    private boolean closest(int number) {
-      return this.radix <= number;
-    }
   }
 }
