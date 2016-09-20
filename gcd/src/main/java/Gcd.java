da302877c7c69dae7cfcafc00497598834071007
holi-java <32414154@qq.com>
2016-09-20 19:08:55 +0800

    rename method

--- gcd/src/main/java/Gcd.java
+++ gcd/src/main/java/Gcd.java
@@ -2,7 +2,7 @@
  * Created by selonj on 16-9-20.
  */
 public class Gcd {
-  public static int of(int a, int b) {
+  public static int signed(int a, int b) {
     return sign(a) * sign(b) * unsigned(a, b);
   }
 
--- gcd/src/test/java/GcdTest.java
+++ gcd/src/test/java/GcdTest.java
@@ -8,36 +8,36 @@ import static org.junit.Assert.assertThat;
  */
 public class GcdTest {
   @Test public void ones() throws Exception {
-    assertThat(Gcd.of(1, 1), equalTo(1));
+    assertThat(Gcd.signed(1, 1), equalTo(1));
   }
 
   @Test public void zeros() throws Exception {
-    assertThat(Gcd.of(0, 0), equalTo(0));
+    assertThat(Gcd.signed(0, 0), equalTo(0));
   }
 
   @Test public void oneOfTheCommonDivisor() throws Exception {
-    assertThat(Gcd.of(4, 2), equalTo(2));
-    assertThat(Gcd.of(2, 4), equalTo(2));
+    assertThat(Gcd.signed(4, 2), equalTo(2));
+    assertThat(Gcd.signed(2, 4), equalTo(2));
   }
 
   @Test public void ascendantInputs() throws Exception {
-    assertThat(Gcd.of(6, 4), equalTo(2));
+    assertThat(Gcd.signed(6, 4), equalTo(2));
   }
 
   @Test public void descendantInputs() throws Exception {
-    assertThat(Gcd.of(4, 6), equalTo(2));
+    assertThat(Gcd.signed(4, 6), equalTo(2));
   }
 
   @Test public void negative() throws Exception {
-    assertThat(Gcd.of(-1, 2), equalTo(-1));
-    assertThat(Gcd.of(-2, 1), equalTo(-1));
+    assertThat(Gcd.signed(-1, 2), equalTo(-1));
+    assertThat(Gcd.signed(-2, 1), equalTo(-1));
   }
 
   @Test public void negative2() throws Exception {
-    assertThat(Gcd.of(1, -2), equalTo(-1));
+    assertThat(Gcd.signed(1, -2), equalTo(-1));
   }
 
   @Test public void negatives() throws Exception {
-    assertThat(Gcd.of(-1, -1), equalTo(1));
+    assertThat(Gcd.signed(-1, -1), equalTo(1));
   }
 }
