d6207e25eb3b991d60157368d776da75646d9dc7
holi-java <32414154@qq.com>
2016-09-20 22:46:45 +0800

    make the n final to see where was changed it value

--- factorial/src/main/java/Factorial.java
+++ factorial/src/main/java/Factorial.java
@@ -2,7 +2,7 @@
  * Created by selonj on 16-9-20.
  */
 public class Factorial {
-  public static int of(int n) {
+  public static int of(final int n) {
     int i = 2;
     if (i < n) {
       n *= i;
