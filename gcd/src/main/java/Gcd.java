8b25123ec21f67ef1eac7032112c48f573452166
holi-java <32414154@qq.com>
2016-09-20 18:51:44 +0800

    remove code duplication

--- gcd/src/main/java/Gcd.java
+++ gcd/src/main/java/Gcd.java
@@ -5,11 +5,10 @@ public class Gcd {
   public static int of(int a, int b) {
     while (b != 0) {
       int tmp = a % b;
+      a = b;
       if (tmp == 0) {
-        a = b;
         break;
       } else {
-        a = b;
         b = tmp;
       }
     }
