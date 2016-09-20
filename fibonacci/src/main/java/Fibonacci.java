14183e17f0e1edfcb58123b9b77835bde1d47263
holi-java <32414154@qq.com>
2016-09-20 22:16:14 +0800

    surprise, that I can replace prev=0 and i=1

--- fibonacci/src/main/java/Fibonacci.java
+++ fibonacci/src/main/java/Fibonacci.java
@@ -3,9 +3,9 @@
  */
 public class Fibonacci {
   public static int of(int n) {
-    int prev = 1;
+    int prev = 0;
     int result = 1;
-    for (int i = 2, tmp; i < n; i++) {
+    for (int i = 1, tmp; i < n; i++) {
       tmp = result;
       result += prev;
       prev = tmp;
