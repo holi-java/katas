1713456345389fa529be0718a467e21a74be7b9f
holi-java <32414154@qq.com>
2016-09-20 22:18:12 +0800

    declare loop variables in for(..)

--- fibonacci/src/main/java/Fibonacci.java
+++ fibonacci/src/main/java/Fibonacci.java
@@ -3,9 +3,8 @@
  */
 public class Fibonacci {
   public static int of(int n) {
-    int prev = 0;
     int result = 1;
-    for (int i = 1, tmp; i < n; i++) {
+    for (int prev = 0, i = 1, tmp; i < n; i++) {
       tmp = result;
       result += prev;
       prev = tmp;
