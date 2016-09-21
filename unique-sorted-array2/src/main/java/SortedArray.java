85935ddcc4834177ee747822b23cdece2df8472d
holi-java <32414154@qq.com>
2016-09-21 10:31:32 +0800

    extract method

--- unique-sorted-array2/src/main/java/SortedArray.java
+++ unique-sorted-array2/src/main/java/SortedArray.java
@@ -3,7 +3,14 @@
  */
 public class SortedArray {
   public static int[] unique(int[] array) {
-    int n = array.length;
+    int n = 0;
+    for (int i = 0; i < array.length; i++){
+      n++;
+    }
+    return copy(array, n);
+  }
+
+  private static int[] copy(int[] array, int n) {
     int[] result = new int[n];
     System.arraycopy(array, 0, result, 0, n);
     return result;
