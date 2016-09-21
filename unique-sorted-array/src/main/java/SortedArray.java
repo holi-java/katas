0727bf8d8e3dfdb0058a9509a23ddc5e95a72e11
holi-java <32414154@qq.com>
2016-09-21 09:34:24 +0800

    flip '>' to '<'

--- unique-sorted-array/src/main/java/SortedArray.java
+++ unique-sorted-array/src/main/java/SortedArray.java
@@ -6,7 +6,7 @@ public class SortedArray {
   public static int[] unique(int[] array) {
     int[] result = new int[array.length];
 
-    for (int i = 0; array.length > i; i++) {
+    for (int i = 0; i < array.length; i++) {
       result[i] = array[i];
     }
     return result;
