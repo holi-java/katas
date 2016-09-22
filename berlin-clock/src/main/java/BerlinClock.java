ed352888a2bf5cf5d789f1639c3e4441a087c716
holi-java <32414154@qq.com>
2016-09-23 06:10:18 +0800

    introduce constants to make code clearly

--- berlin-clock/src/main/java/BerlinClock.java
+++ berlin-clock/src/main/java/BerlinClock.java
@@ -2,9 +2,14 @@
  * Created by selonj on 16-9-23.
  */
 public class BerlinClock {
-  private static final Color[] HOURS_LIGHTS = {Color.RED, Color.RED, Color.RED, Color.RED};
-  private static final Color[] TOP_MINUTES_LIGHTS = {Color.YELLOW, Color.YELLOW, Color.RED, Color.YELLOW, Color.YELLOW, Color.RED, Color.YELLOW, Color.YELLOW, Color.RED, Color.YELLOW, Color.YELLOW};
-  private static final Color[] BOTTOM_MINUTES_LIGHTS = {Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW};
+  private static final Color RED = Color.RED;
+  private static final Color YELLOW = Color.YELLOW;
+  private static final Color OFF = Color.BLACK;
+
+  private static final Color SECOND_LIGHT = YELLOW;
+  private static final Color[] HOURS_LIGHTS = {RED, RED, RED, RED};
+  private static final Color[] TOP_MINUTES_LIGHTS = {YELLOW, YELLOW, RED, YELLOW, YELLOW, RED, YELLOW, YELLOW, RED, YELLOW, YELLOW};
+  private static final Color[] BOTTOM_MINUTES_LIGHTS = {YELLOW, YELLOW, YELLOW, YELLOW};
   private final int hours;
   private final int minutes;
   private int seconds;
@@ -20,7 +25,7 @@ public class BerlinClock {
   }
 
   public String display() {
-    return light(Color.YELLOW, seconds % 2 == 0) + "\n" +
+    return light(SECOND_LIGHT, seconds % 2 == 0) + "\n" +
         /**/lights(HOURS_LIGHTS, hours / 5) + "\n" +
         /**/lights(HOURS_LIGHTS, hours % 5) + "\n" +
         /**/lights(TOP_MINUTES_LIGHTS, minutes / 5) + "\n" +
@@ -35,6 +40,6 @@ public class BerlinClock {
   }
 
   private String light(Color color, boolean lit) {
-    return (lit ? color : Color.OFF).code;
+    return (lit ? color : OFF).code;
   }
 }
--- berlin-clock/src/main/java/Color.java
+++ berlin-clock/src/main/java/Color.java
@@ -2,7 +2,7 @@
  * Created by selonj on 16-9-23.
  */
 public enum Color {
-  YELLOW("Y"), RED("R"), OFF("0");
+  YELLOW("Y"), RED("R"), BLACK("0");
 
   public final String code;
 
