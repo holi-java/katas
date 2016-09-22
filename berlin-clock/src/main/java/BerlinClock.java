5baf6500c4664852d5f718bbf03e864e9c7bdb3f
holi-java <32414154@qq.com>
2016-09-23 06:01:53 +0800

    change BOTTOM_MINUTES_LIGHTS type to Color[]

--- berlin-clock/src/main/java/BerlinClock.java
+++ berlin-clock/src/main/java/BerlinClock.java
@@ -6,7 +6,7 @@ public class BerlinClock {
   private static final String RED = "R";
   private static final String OFF = "0";
   private static final Color[] HOURS_LIGHTS = {Color.RED, Color.RED, Color.RED, Color.RED};
-  private static final String[] BOTTOM_MINUTES_LIGHTS = {YELLOW, YELLOW, YELLOW, YELLOW};
+  private static final Color[] BOTTOM_MINUTES_LIGHTS = {Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW};
   private static final String[] TOP_MINUTES_LIGHTS = {YELLOW, YELLOW, RED, YELLOW, YELLOW, RED, YELLOW, YELLOW, RED, YELLOW, YELLOW};
   private final int hours;
   private final int minutes;
