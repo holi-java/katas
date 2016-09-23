c785aaffaf7d48a7c97bde0088e951804ee67567
holi-java <32414154@qq.com>
2016-09-23 12:38:03 +0800

    merge common range tests

--- range/src/test/java/RangeTest.java
+++ range/src/test/java/RangeTest.java
@@ -1,23 +1,26 @@
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by selonj on 16-9-23.
  */
-public class RangeTest {
+public class RangeTest extends AbstractRangeTest {
+  public RangeTest(int start, int last) {
+    super(start, last);
+  }
+
+  @Test public void size() throws Exception {
+    int expectedSize = Math.abs(last - start + range.direction());
+    assertThat(range.size(), equalTo(expectedSize));
+  }
 
   @Test public void start() throws Exception {
-    assertThat(Range.between(0, 10).start(), equalTo(0));
+    assertThat(range.start(), equalTo(start));
   }
 
   @Test public void last() throws Exception {
-    assertThat(Range.between(0, 10).last(), equalTo(10));
-  }
-
-  @Test public void direction() throws Exception {
-    assertThat(Range.between(0, 10).direction(), equalTo(1));
-    assertThat(Range.between(10, 0).direction(), equalTo(-1));
+    assertThat(range.last(), equalTo(last));
   }
 }
