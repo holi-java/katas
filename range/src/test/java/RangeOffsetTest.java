a7a4505e431650c36f9409086c83b142e1b8c563
holi-java <32414154@qq.com>
2016-09-23 13:01:29 +0800

    I was very embarrassed,that I turn a big circle in the circles.

--- range/src/test/java/RangeOffsetTest.java
+++ range/src/test/java/RangeOffsetTest.java
@@ -32,4 +32,12 @@ public class RangeOffsetTest extends AbstractRangeTest {
   @Test public void beforeStart() throws Exception {
     assertThat(range.offset(range.start() - range.direction()), equalTo(range.size() - 1));
   }
+
+  @Test public void beforeStart2() throws Exception {
+    assertThat(range.offset(range.start() - range.size() * range.direction() - range.direction()), equalTo(range.size() - 1));
+  }
+
+  @Test public void afterLast2() throws Exception {
+    assertThat(range.offset(range.last() + range.size() * range.direction() + range.direction()), equalTo(0));
+  }
 }
