f8f44ee39c45d9f8331b1c04def466f4d67e31b4
holi-java <32414154@qq.com>
2016-09-23 09:56:22 +0800

    introduce 'range' field in test

--- range/src/test/java/RangeRoundingTest.java
+++ range/src/test/java/RangeRoundingTest.java
@@ -8,20 +8,27 @@ import static org.junit.Assert.assertThat;
  * Created by selonj on 16-9-23.
  */
 public class RangeRoundingTest {
+
+  private final Range range;
+
+  public RangeRoundingTest() {
+    range = Range.between(0, 10);
+  }
+
   @Test public void startValue() throws Exception {
-    assertThat(Range.between(0, 10).round(0), equalTo(0));
+    assertThat(range.round(range.start()), equalTo(range.start()));
   }
 
   @Test public void lastValue() throws Exception {
-    assertThat(Range.between(0, 10).round(10), equalTo(10));
+    assertThat(range.round(range.last()), equalTo(range.last()));
   }
 
   @Test public void afterLastValueShouldBeRounded() throws Exception {
-    assertThat(Range.between(0, 10).round(11), equalTo(0));
+    assertThat(range.round(range.last() + 1), equalTo(range.start()));
   }
 
   @Test public void beforeStartValueShouldBeRounded() throws Exception {
-    assertThat(Range.between(0, 10).round(-1), equalTo(10));
-    assertThat(Range.between(0, 10).round(-1 - 11), equalTo(10));
+    assertThat(range.round(range.start() - 1), equalTo(range.last()));
+    assertThat(range.round(-1 - range.size()), equalTo(range.last()));
   }
 }
