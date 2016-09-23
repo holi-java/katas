54b6eaf57dbe759129b1f16b1701dfbb30b7afc3
holi-java <32414154@qq.com>
2016-09-23 13:18:33 +0800

    add lost tests & resort tests order by position

--- range/src/test/java/RangeOffsetTest.java
+++ range/src/test/java/RangeOffsetTest.java
@@ -13,6 +13,14 @@ public class RangeOffsetTest extends AbstractRangeTest {
     super(start, last);
   }
 
+  @Test public void beforeStart2() throws Exception {
+    assertThat(range.offset(range.start() - range.size() * range.direction() - range.direction()), equalTo(range.size() - 1));
+  }
+
+  @Test public void beforeStart() throws Exception {
+    assertThat(range.offset(range.start() - range.direction()), equalTo(range.size() - 1));
+  }
+
   @Test public void start() throws Exception {
     assertThat(range.offset(range.start()), equalTo(0));
   }
@@ -21,20 +29,16 @@ public class RangeOffsetTest extends AbstractRangeTest {
     assertThat(range.offset(range.start() + range.direction()), equalTo(1));
   }
 
+  @Test public void beforeLast() throws Exception {
+    assertThat(range.offset(range.last() - range.direction()), equalTo(range.size() - 2));
+  }
+
   @Test public void last() throws Exception {
     assertThat(range.offset(range.last()), equalTo(range.size() - 1));
   }
 
   @Test public void afterLast() throws Exception {
     assertThat(range.offset(range.last() + range.direction()), equalTo(0));
-  }
-
-  @Test public void beforeStart() throws Exception {
-    assertThat(range.offset(range.start() - range.direction()), equalTo(range.size() - 1));
-  }
-
-  @Test public void beforeStart2() throws Exception {
-    assertThat(range.offset(range.start() - range.size() * range.direction() - range.direction()), equalTo(range.size() - 1));
   }
 
   @Test public void afterLast2() throws Exception {
--- range/src/test/java/RangeRoundingTest.java
+++ range/src/test/java/RangeRoundingTest.java
@@ -12,20 +12,35 @@ public class RangeRoundingTest extends AbstractRangeTest {
     super(start, last);
   }
 
-  @Test public void startValue() throws Exception {
+  @Test public void beforeStart2() throws Exception {
+    assertThat(range.round(range.start() - range.direction() * range.size() - range.direction()), equalTo(range.last()));
+  }
+
+  @Test public void beforeStart() throws Exception {
+    assertThat(range.round(range.start() - range.direction()), equalTo(range.last()));
+  }
+
+  @Test public void start() throws Exception {
     assertThat(range.round(range.start()), equalTo(range.start()));
   }
 
-  @Test public void lastValue() throws Exception {
+  @Test public void next() throws Exception {
+    assertThat(range.round(range.start() + range.direction()), equalTo(range.start() + range.direction()));
+  }
+
+  @Test public void beforeLast() throws Exception {
+    assertThat(range.round(range.last() - range.direction()), equalTo(range.last() - range.direction()));
+  }
+
+  @Test public void last() throws Exception {
     assertThat(range.round(range.last()), equalTo(range.last()));
   }
 
-  @Test public void afterLastValueShouldBeRounded() throws Exception {
+  @Test public void afterLast() throws Exception {
     assertThat(range.round(range.last() + range.direction()), equalTo(range.start()));
   }
 
-  @Test public void beforeStartValueShouldBeRounded() throws Exception {
-    assertThat(range.round(range.start() - range.direction()), equalTo(range.last()));
-    assertThat(range.round(range.start() - range.direction() - range.direction() * range.size()), equalTo(range.last()));
+  @Test public void afterLast2() throws Exception {
+    assertThat(range.round(range.last() + range.direction() * range.size() + range.direction()), equalTo(range.start()));
   }
 }
