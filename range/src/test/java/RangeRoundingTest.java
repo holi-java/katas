3db392fd3c6d983fac8fb33ca5b0b7813a4b3bd0
holi-java <32414154@qq.com>
2016-09-23 10:11:30 +0800

    run tests as Parameterized

--- range/src/test/java/RangeRoundingTest.java
+++ range/src/test/java/RangeRoundingTest.java
@@ -1,5 +1,8 @@
-import org.junit.Ignore;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -7,12 +10,20 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by selonj on 16-9-23.
  */
+@RunWith(Parameterized.class)
 public class RangeRoundingTest {
 
   private final Range range;
 
-  public RangeRoundingTest() {
-    range = Range.between(0, 10);
+  public RangeRoundingTest(Range range) {
+    this.range = range;
+  }
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        new Object[] {Range.between(0, 10)}
+    });
   }
 
   @Test public void startValue() throws Exception {
